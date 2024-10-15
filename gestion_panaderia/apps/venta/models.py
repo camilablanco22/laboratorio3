d3098c6cc0459400c82360779d0b286e3449867d
Camila Blanco <blancocamila2003@gmail.com>
2024-10-15 17:03:23 -0300

    Agrego los modelos para las ventas

--- gestion_panaderia/apps/venta/models.py
+++ gestion_panaderia/apps/venta/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Venta(models.Model):
+    tipo_cliente = models.CharField()
+    #Espacio para la clave foránea de cliente_mayorista
+    fecha_venta = models.DateTimeField()
+    tipo_venta = models.CharField()
+    forma_pago = models.CharField()
+    monto_total = models.DecimalField(max_digits = 10, decimal_places = 2)
+    observaciones = models.CharField(max_length = 500)
+    #espacio para la clave foránea del empleado que realizó la venta
+
+
+class Item(models.Model):
+    #Espacio para clave foranea que relaciona con el producto
+    cantidad = models.DecimalField(max_digits = 10, decimal_places = 2)
+    precio_item = models.DecimalField(max_digits = 10, decimal_places = 2)
+
