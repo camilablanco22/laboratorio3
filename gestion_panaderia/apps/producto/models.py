a9eb0b6b6f7be4a82c02fcabc1f93d98e0add2ab
Camila Blanco <blancocamila2003@gmail.com>
2024-10-19 20:26:03 -0300

    Creo forms, vistas, urls y templates para agregar producto

--- gestion_panaderia/apps/producto/models.py
+++ gestion_panaderia/apps/producto/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
+    UNIDAD_MEDIDA_CHOICES = [
+        ('kg', 'Kilogramos'),
+        ('unid', 'Unidades')
+    ]
+
+    TIPO_CHOICES = [
+        ('pasteleria', 'Pasteleria'),
+        ('panaderia', 'Panaderia')
+    ]
+
     nombre = models.CharField(max_length=150, unique=True)
     descripcion = models.CharField(max_length=500)
-    unidadMedida = models.CharField(max_length=50)
+    unidadMedida = models.CharField(max_length=4, choices=UNIDAD_MEDIDA_CHOICES)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.PositiveIntegerField(max_length=10)
-    tipo = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50, choices=TIPO_CHOICES)
     estado = models.BooleanField(default=True)
 
     def __str__(self):
