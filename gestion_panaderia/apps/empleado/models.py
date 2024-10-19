2df94ad732fb65672e2044dcc21063fed4da0bbc
Antonella rosales <rosalesantonella4@gmail.com>
2024-10-19 15:41:14 -0300

    Modificacion modelos cliente_mayorista y Empleado

--- gestion_panaderia/apps/empleado/models.py
+++ gestion_panaderia/apps/empleado/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#from apps.usuario.models import Usuario
 
 
 class Empleado(models.Model):
@@ -12,25 +13,9 @@ class Empleado(models.Model):
     domicilio_numero = models.IntegerField()
     domicilio_localidad = models.CharField(max_length=50)
     domicilio_departamento = models.CharField(max_length=50)
+    #usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre_completo
 
-    def dar_baja(self):
-        self.estado = False
-        self.save()
 
-    def modificar(self, nombre=None, email=None, calle=None, numero=None, localidad=None, departamento=None):
-        if nombre:
-            self.nombre_completo = nombre
-        if email:
-            self.email = email
-        if calle:
-            self.domicilio_calle = calle
-        if numero:
-            self.domicilio_numero = numero
-        if localidad:
-            self.domicilio_localidad = localidad
-        if departamento:
-            self.domicilio_departamento = departamento
-        self.save()
