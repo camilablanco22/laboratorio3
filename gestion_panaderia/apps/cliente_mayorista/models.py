4ddc9dbf4b28ce7da28d7216b3617ba03d03f93c
Antonella rosales <rosalesantonella4@gmail.com>
2024-10-19 13:05:43 -0300

    Modelos Empleado y ClienteMayorista

--- gestion_panaderia/apps/cliente_mayorista/models.py
+++ gestion_panaderia/apps/cliente_mayorista/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class ClienteMayorista(models.Model):
+    nombre_completo = models.CharField(max_length=100)
+    cuit = models.CharField(max_length=13, unique=True)
+    activo = models.BooleanField(default=True)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+    domicilio_calle = models.CharField(max_length=100)
+    domicilio_numero = models.IntegerField()
+    localidad = models.CharField(max_length=50)
+    departamento = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_completo
+
+    def dar_baja(self):
+        self.activo = False
+        self.save()
+
+    def modificar(self, nombre=None, calle=None, numero=None, localidad=None, departamento=None):
+        if nombre:
+            self.nombre_completo = nombre
+        if calle:
+            self.domicilio_calle = calle
+        if numero:
+            self.domicilio_numero = numero
+        if localidad:
+            self.localidad = localidad
+        if departamento:
+            self.departamento = departamento
+        self.save()
--- gestion_panaderia/apps/empleado/models.py
+++ gestion_panaderia/apps/empleado/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Empleado(models.Model):
+    estado = models.BooleanField(default=True)
+    cuit = models.CharField(max_length=13, unique=True)
+    nombre_completo = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    fecha_nacimiento = models.DateField()
+    fecha_incorporacion = models.DateField()
+    domicilio_calle = models.CharField(max_length=100)
+    domicilio_numero = models.IntegerField()
+    domicilio_localidad = models.CharField(max_length=50)
+    domicilio_departamento = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_completo
+
+    def dar_baja(self):
+        self.estado = False
+        self.save()
+
+    def modificar(self, nombre=None, email=None, calle=None, numero=None, localidad=None, departamento=None):
+        if nombre:
+            self.nombre_completo = nombre
+        if email:
+            self.email = email
+        if calle:
+            self.domicilio_calle = calle
+        if numero:
+            self.domicilio_numero = numero
+        if localidad:
+            self.domicilio_localidad = localidad
+        if departamento:
+            self.domicilio_departamento = departamento
+        self.save()
