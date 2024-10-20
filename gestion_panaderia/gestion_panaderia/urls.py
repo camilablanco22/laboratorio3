18b78671a40908ae76a4f29f1c7e6e0ab706ea51
Camila Blanco <blancocamila2003@gmail.com>
2024-10-20 11:21:54 -0300

    Configuro home

--- gestion_panaderia/gestion_panaderia/urls.py
+++ gestion_panaderia/gestion_panaderia/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('producto/', include('apps.producto.urls', namespace='producto'))
-]
+    path('producto/', include('apps.producto.urls', namespace='producto')),
+    path('', TemplateView.as_view(template_name='base/home.html'), name='home'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
