f2aa9df6d836f676e9bf97f1050548e61154a860
Donato Costa <donatocosta71@gmail.com>
2024-10-27 15:44:52 -0300

    ABM COMPLETO DE PROVEEDOR 3

--- gestion_panaderia/apps/proveedor/models.py
+++ gestion_panaderia/apps/proveedor/models.py
@@ -11,3 +11,6 @@ class Proveedor(models.Model):
     domicilioDepartamento = models.CharField(max_length=100)
     razonSocial = models.CharField(max_length=100)
     estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
